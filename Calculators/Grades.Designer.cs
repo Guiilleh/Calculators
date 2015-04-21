8b664a8ba7d2ecfc544ebea3351bba0019ea71b3
Guiilleh <fguille94@outlook.com>
2015-04-21 17:36:54 -0430

    Fixing resources

---
+++ Calculators/Grades.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Calculators
+{
+    partial class Grades
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Grades));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelresult = new System.Windows.Forms.Label();
+            this.equal = new System.Windows.Forms.Label();
+            this.corte1 = new System.Windows.Forms.TextBox();
+            this.corte2 = new System.Windows.Forms.TextBox();
+            this.corte3 = new System.Windows.Forms.TextBox();
+            this.result1 = new System.Windows.Forms.TextBox();
+            this.result2 = new System.Windows.Forms.TextBox();
+            this.calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1er Corte";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "2do Corte";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(160, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "3er Corte";
+            // 
+            // labelresult
+            // 
+            this.labelresult.AutoSize = true;
+            this.labelresult.Location = new System.Drawing.Point(273, 23);
+            this.labelresult.Name = "labelresult";
+            this.labelresult.Size = new System.Drawing.Size(51, 13);
+            this.labelresult.TabIndex = 3;
+            this.labelresult.Text = "Definitiva";
+            // 
+            // equal
+            // 
+            this.equal.AutoSize = true;
+            this.equal.Location = new System.Drawing.Point(300, 47);
+            this.equal.Name = "equal";
+            this.equal.Size = new System.Drawing.Size(13, 13);
+            this.equal.TabIndex = 4;
+            this.equal.Text = "=";
+            // 
+            // corte1
+            // 
+            this.corte1.BackColor = System.Drawing.SystemColors.Window;
+            this.corte1.Location = new System.Drawing.Point(37, 47);
+            this.corte1.MaxLength = 3;
+            this.corte1.Name = "corte1";
+            this.corte1.Size = new System.Drawing.Size(46, 20);
+            this.corte1.TabIndex = 5;
+            this.corte1.Text = "0";
+            this.corte1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.corte1.TextChanged += new System.EventHandler(this.MainMGC1_Run);
+            this.corte1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown01);
+            this.corte1.Leave += new System.EventHandler(this.MainMGC1_Run);
+            this.corte1.MouseHover += new System.EventHandler(this.corte1_MouseHover);
+            // 
+            // corte2
+            // 
+            this.corte2.BackColor = System.Drawing.SystemColors.Window;
+            this.corte2.Location = new System.Drawing.Point(102, 47);
+            this.corte2.MaxLength = 3;
+            this.corte2.Name = "corte2";
+            this.corte2.Size = new System.Drawing.Size(46, 20);
+            this.corte2.TabIndex = 6;
+            this.corte2.Text = "0";
+            this.corte2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.corte2.TextChanged += new System.EventHandler(this.MainMGC1_Run);
+            this.corte2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown02);
+            this.corte2.Leave += new System.EventHandler(this.MainMGC1_Run);
+            this.corte2.MouseHover += new System.EventHandler(this.corte2_MouseHover);
+            // 
+            // corte3
+            // 
+            this.corte3.BackColor = System.Drawing.SystemColors.Window;
+            this.corte3.Location = new System.Drawing.Point(163, 47);
+            this.corte3.MaxLength = 3;
+            this.corte3.Name = "corte3";
+            this.corte3.Size = new System.Drawing.Size(46, 20);
+            this.corte3.TabIndex = 7;
+            this.corte3.Text = "0";
+            this.corte3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.corte3.TextChanged += new System.EventHandler(this.MainMGC1_Run);
+            this.corte3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown03);
+            this.corte3.Leave += new System.EventHandler(this.MainMGC1_Run);
+            this.corte3.MouseHover += new System.EventHandler(this.corte3_MouseHover);
+            // 
+            // result1
+            // 
+            this.result1.BackColor = System.Drawing.SystemColors.Window;
+            this.result1.Location = new System.Drawing.Point(246, 47);
+            this.result1.MaxLength = 6;
+            this.result1.Name = "result1";
+            this.result1.ReadOnly = true;
+            this.result1.Size = new System.Drawing.Size(46, 20);
+            this.result1.TabIndex = 8;
+            this.result1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.result1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown040506);
+            this.result1.MouseHover += new System.EventHandler(this.result1_MouseHover);
+            // 
+            // result2
+            // 
+            this.result2.BackColor = System.Drawing.SystemColors.Window;
+            this.result2.Location = new System.Drawing.Point(319, 47);
+            this.result2.MaxLength = 2;
+            this.result2.Name = "result2";
+            this.result2.ReadOnly = true;
+            this.result2.Size = new System.Drawing.Size(31, 20);
+            this.result2.TabIndex = 9;
+            this.result2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.result2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown040506);
+            this.result2.MouseHover += new System.EventHandler(this.result2_MouseHover);
+            // 
+            // calcular
+            // 
+            this.calcular.Location = new System.Drawing.Point(220, 85);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(75, 38);
+            this.calcular.TabIndex = 10;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.MainMGC1_Run);
+            this.calcular.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyDown040506);
+            // 
+            // Grades
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 142);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.result2);
+            this.Controls.Add(this.result1);
+            this.Controls.Add(this.corte3);
+            this.Controls.Add(this.corte2);
+            this.Controls.Add(this.corte1);
+            this.Controls.Add(this.equal);
+            this.Controls.Add(this.labelresult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Grades";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculador de Notas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelresult;
+        private System.Windows.Forms.Label equal;
+        private System.Windows.Forms.TextBox corte1;
+        private System.Windows.Forms.TextBox corte3;
+        private System.Windows.Forms.TextBox corte2;
+        private System.Windows.Forms.TextBox result1;
+        private System.Windows.Forms.TextBox result2;
+        private System.Windows.Forms.Button calcular;
+    }
+}
---
+++ Calculators/Grades.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculators
+{
+    public partial class Grades : Form
+    {
+        public Grades()
+        {
+            InitializeComponent();
+        }
+
+        #region Main Method Grades Calculator 1
+        public void MainMGC1()
+        {
+            try
+            {
+                Double corte01 = Double.Parse(corte1.Text);
+                Double corte02 = Double.Parse(corte2.Text);
+                Double corte03 = Double.Parse(corte3.Text);
+
+                if (corte01 > 20)
+                {
+                    corte1.Text = "20";
+                }
+                else if (corte01 < 0)
+                {
+                    corte1.Text = "0";
+                }
+                else 
+                {
+                    corte1.Text = corte1.Text;
+                }
+
+                if (corte02 > 20)
+                {
+                    corte2.Text = "20";
+                }
+                else if (corte02 < 0)
+                {
+                    corte2.Text = "0";
+                }
+                else
+                {
+                    corte2.Text = corte2.Text;
+                }
+
+                if (corte03 > 20)
+                {
+                    corte3.Text = "20";
+                }
+                else if (corte03 < 0)
+                {
+                    corte3.Text = "0";
+                }
+                else
+                {
+                    corte3.Text = corte3.Text;
+                }
+
+                Double result = (corte01 > 20 ? 20 : corte01) * 0.3 + (corte02 > 20 ? 20 : corte02) * 0.3 + (corte03 > 20 ? 20 : corte03) * 0.4;
+                Double result01 = result;
+                Double result02 = result;
+
+                String resultA = result01.ToString("0.##");
+                String resultB = result02.ToString("0");
+
+                result1.Text = resultA;
+                result2.Text = resultB;
+            }
+            catch
+            {
+                MessageBox.Show("Sólo números",
+                    "Atención",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+            }
+        }//MainMethodGC1(); END
+        #endregion
+        
+        #region KeyDown Methods
+        private void KeyDown01(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                int asd1 = Int32.Parse(corte1.Text) + 1;
+                corte1.Text = asd1 + "";
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                int asd1 = Int32.Parse(corte1.Text) - 1;
+                corte1.Text = asd1 + "";
+            }
+        }
+        private void KeyDown02(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                int asd2 = Int32.Parse(corte2.Text) + 1;
+                corte2.Text = asd2 + "";
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                int asd2 = Int32.Parse(corte2.Text) - 1;
+                corte2.Text = asd2 + "";
+            }
+        }
+        private void KeyDown03(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                int asd3 = Int32.Parse(corte3.Text) + 1;
+                corte3.Text = asd3 + "";
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                int asd3 = Int32.Parse(corte3.Text) - 1;
+                corte3.Text = asd3 + "";
+            }
+        }
+        private void KeyDown040506(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+        #endregion
+
+        #region ToolTips when hovering mouse
+        private void corte1_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip TT = new ToolTip();
+            TT.SetToolTip(this.corte1, "Primer corte de notas");
+        }
+        private void corte2_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip TT = new ToolTip();
+            TT.SetToolTip(this.corte2, "Segundo corte de notas");
+        }
+        private void corte3_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip TT = new ToolTip();
+            TT.SetToolTip(this.corte3, "Tercer corte de notas");
+        }
+        private void result1_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip TT = new ToolTip();
+            TT.SetToolTip(this.result1, "Nota definitiva exacta");
+        }
+        private void result2_MouseHover(object sender, EventArgs e)
+        {
+            ToolTip TT = new ToolTip();
+            TT.SetToolTip(this.result2, "Nota definitiva redondeada \nSin decimales");
+        }
+        #endregion
+
+        #region Methods_Run
+        private void MainMGC1_Run(object sender, EventArgs e)
+        {
+            MainMGC1();
+        }
+        #endregion
+    }
+}
